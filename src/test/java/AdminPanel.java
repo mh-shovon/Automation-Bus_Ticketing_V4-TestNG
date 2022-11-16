3564e6dde13c2a8b8b426d2a4551e8a5d121fd52
Shovon <shovon33266@gmail.com>
2022-11-16 12:48:49 +0600

    Add Admin Panel

---
+++ src/test/java/AdminPanel.java
@@ -0,0 +1,232 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AdminPanel {
+    @FindBy(id = "phone")
+    WebElement txtPhone;
+    @FindBy(id = "password")
+    WebElement txtPassword;
+    @FindBy(xpath = "//*[@id=\"app\"]/main/div/div/div/div[2]/form/button")
+    WebElement btnLogin;
+
+    @FindBy(xpath = "//a[@id='navbarScrollingDropdown']")
+    WebElement btnAdminLogoutDisplayed;
+
+    @FindBy(xpath = "//button[contains(text(),'Logout')]")
+    WebElement btnAdminLogout;
+
+    @FindBy(xpath = "//a[@id='navbarScrollingDropdown']")
+    WebElement btnCountermanLogoutDisplayed;
+
+    @FindBy(xpath = "//header/nav[1]/div[1]/div[1]/ul[2]/li[1]/ul[1]/li[1]/button[1]")
+    WebElement btnCountermanLogout;
+
+    @FindBy(xpath = "//a[contains(text(),'Dashboard')]")
+    WebElement btnAdminDashboard;
+
+    @FindBy(xpath = "//a[contains(text(),'Report')]")
+    WebElement btnClickReport;
+
+    @FindBy(xpath = "//a[contains(text(),'Admins')]")
+    WebElement btnClickAdmins;
+
+    @FindBy(xpath = "//a[contains(text(),'Company Deploy')]")
+    WebElement btnClickCompanyDeploy;
+
+    @FindBy(xpath = "//a[contains(text(),'Digital Ticketing')]")
+    WebElement btnClickDigitalTicketing;
+
+    @FindBy(xpath = "//button[@id='admin-report-coach-report-tab']")
+    WebElement btnAdminCoachWiseReport;
+
+    @FindBy(xpath = "//button[@id='admin-report-counter-report-tab']")
+    WebElement btnAdminCounterWiseReport;
+
+    @FindBy(xpath = "//button[@id='admin-report-agent-report-tab']")
+    WebElement btnAdminAgentWiseReport;
+
+    @FindBy(xpath = "//button[@id='admin-report-print-count-report-tab']")
+    WebElement btnAdminUserWiseReport;
+
+    @FindBy(xpath = "//button[@id='admin-report-reservation-report-tab']")
+    WebElement btnAdminReservationReport;
+
+    @FindBy(xpath = "//select[@id='company']")
+    WebElement btnSelectCompany;
+
+    @FindBy(xpath = "//*[@id=\"company\"]/option[43]")
+    WebElement btnCompanyNameSelect;
+
+    @FindBy(xpath = "//body/div[@id='app']/main[1]/div[1]/div[1]/div[1]/div[1]/div[6]/div[1]/section[1]/div[2]/div[1]/div[2]/div[1]/div[2]/button[1]")
+    WebElement btnFilter;
+
+    @FindBy(xpath = "//button[contains(text(),'Seat')]")
+    WebElement btnViewSeatTicket;
+
+    //***** Company Deploy *****//
+    @FindBy(xpath = "//body/div[@id='app']/main[1]/div[1]/div[1]/div[1]/button[1]")
+    WebElement btnAdd;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/main/div/div/div[2]/div/div/nav/ul/div/nav/ul/li[4]/button")
+    WebElement btnNext;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/main[1]/div[1]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[20]/td[5]/button[2]")
+    WebElement btnManage;
+    //***** Company Deploy *****//
+
+    //***** Trips *****//
+    @FindBy(xpath = "//button[@id='admin-company-deploy-trips-tab']")
+    WebElement btnTrips;
+
+    @FindBy(xpath = "//span[contains(text(),'Add New')]")
+    WebElement btnAddNew;
+    @FindBy(xpath = "//span[contains(text(),'Go Back')]")
+    WebElement btnGoBack;
+    @FindBy(xpath = "//select[@id='route']")
+    WebElement clickRoutDropDown;
+    @FindBy(xpath = "//option[contains(text(),'Dhaka-Patuakhali')]")
+    WebElement setRouteName;
+    @FindBy(xpath = "//*[@id=\"myTabContent\"]/div[15]/form/div/div[1]/div[1]/div[3]/div/input")
+    WebElement clickFromDate;
+    @FindBy(xpath = "//*[@id=\"myTabContent\"]/div[15]/form/div/div[1]/div[1]/div[3]/div/div/div/div/div[2]/div[1]/div/div[2]/div[38]/span")
+    WebElement setFromDate;
+    @FindBy(xpath = "//body/div[@id='app']/main[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[15]/form[1]/div[1]/div[1]/div[1]/div[3]/div[1]/input[1]")
+    WebElement clickToDate;
+    @FindBy(xpath = "//body/div[@id='app']/main[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[15]/form[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/*[1]")
+    WebElement clickNextDate;
+    @FindBy(xpath = "//body/div[@id='app']/main[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[15]/form[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[41]/span[1]")
+    WebElement setToDate;
+
+    @FindBy(xpath = "//body/div[@id='app']/main[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[15]/form[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/input[1]")
+    WebElement clickTime;
+//    @FindBy(xpath = "")
+
+    //***** Trips *****//
+    WebDriver driver;
+
+    public AdminPanel(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean doAdminLoginWithValidCredentials(String phone, String password) {
+        txtPhone.sendKeys(phone);
+        txtPassword.sendKeys(password);
+        btnLogin.click();
+        return btnAdminLogoutDisplayed.isDisplayed();
+    }
+
+    public boolean clickDashboard() {
+        btnAdminDashboard.click();
+        return btnAdminDashboard.isDisplayed();
+    }
+
+    public boolean clickReport() {
+        btnClickReport.click();
+        return btnClickReport.isDisplayed();
+    }
+
+    public boolean clickAdmins(){
+        btnClickAdmins.click();
+        return btnClickAdmins.isSelected();
+
+    }
+
+    public boolean clickCompanyDeploy(){
+        btnClickCompanyDeploy.click();
+        return btnClickCompanyDeploy.isSelected();
+
+    }
+
+    public boolean clickDigitalTicketing() {
+        btnClickDigitalTicketing.click();
+        return btnClickDigitalTicketing.isSelected();
+    }
+
+    public boolean adminCoachWiseReport() {
+        btnClickReport.click();
+        return btnAdminCoachWiseReport.isDisplayed();
+    }
+
+    public boolean adminCounterWiseReport() {
+        btnClickReport.click();
+        return btnAdminCounterWiseReport.isDisplayed();
+    }
+
+    public boolean adminAgentWiseReport() {
+        btnClickReport.click();
+        return btnAdminAgentWiseReport.isDisplayed();
+    }
+
+    public boolean adminUserWIseReport() {
+        btnClickReport.click();
+        return btnAdminUserWiseReport.isDisplayed();
+    }
+
+    public boolean adminReservationReport() {
+        btnClickReport.click();
+        return btnAdminReservationReport.isDisplayed();
+    }
+
+    public boolean selectCompanyDropDown() {
+        btnSelectCompany.click();
+        return btnSelectCompany.isDisplayed();
+    }
+
+    public boolean companyNameSelect(){
+        btnCompanyNameSelect.click();
+        return btnCompanyNameSelect.isSelected();
+    }
+
+    public boolean adminCoachWiseDataFilter(){
+        btnFilter.click();
+        return btnFilter.isDisplayed();
+    }
+
+    public boolean adminCoachWiseSeatTicketDataFilter(){
+        btnViewSeatTicket.click();
+        return btnViewSeatTicket.isDisplayed();
+    }
+
+    public boolean clickNextButton(){
+        btnNext.click();
+        return btnAdd.isDisplayed();
+    }
+
+    public boolean manageCompany(){
+        btnManage.click();
+        return btnAdd.isDisplayed();
+    }
+
+    public boolean viewTrips(){
+        btnTrips.click();
+        return btnAddNew.isDisplayed();
+    }
+
+    public boolean clickAddNew(){
+        btnAddNew.click();
+        return btnGoBack.isDisplayed();
+    }
+
+    public boolean createTrip(){
+        clickRoutDropDown.click();
+        setRouteName.click();
+        clickFromDate.click();
+        clickNextDate.click();
+        setFromDate.click();
+        clickToDate.click();
+        setToDate.click();
+        return btnGoBack.isDisplayed();
+    }
+
+
+    public boolean doCountermanLoginWithValidCredentials(String phone, String password){
+        txtPhone.sendKeys(phone);
+        txtPassword.sendKeys(password);
+        btnLogin.click();
+        return btnCountermanLogoutDisplayed.isDisplayed();
+    }
+
+}
---
+++ src/test/java/AdminPanelTestRunner.java
@@ -0,0 +1,207 @@
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class AdminPanelTestRunner extends Setup {
+    AdminPanel adminPanel;
+    UtilsAdmin utilsAdmin;
+    UtilsCounterman utilsCounterman;
+    @Test(priority = 1)
+    public void doAdminLoginWithValidCredentials() throws IOException, ParseException, InterruptedException {
+        adminPanel =new AdminPanel(driver);
+        utilsAdmin =new UtilsAdmin();
+        utilsAdmin.getUserCredentials(0);
+        driver.get("https://intercity.jatriweb.team/");
+        boolean isLogoutFound= adminPanel.doAdminLoginWithValidCredentials(utilsAdmin.getPhone(),utilsAdmin.getPassword());
+        Assert.assertEquals(isLogoutFound,true);
+        adminPanel.btnAdminLogoutDisplayed.isDisplayed();
+        Thread.sleep(3000);
+        //loginPage.btnAdminLogout.click();
+    }
+    @Test(priority = 2)
+    public void ClickDashboard() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnAdminDashboard.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 3)
+    public void clickReportV1() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnClickReport.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 4)
+    public void clickAdmins() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnClickAdmins.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 5)
+    public void clickCompanyDeploy() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnClickCompanyDeploy.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 6)
+    public void clickDigitalTicketing() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnClickDigitalTicketing.click();
+        Thread.sleep(1000);
+    }
+    @Test(priority = 7)
+    public void clickReportV2() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnClickReport.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 8)
+    public void adminCoachWiseReportV1() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnAdminCoachWiseReport.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 9)
+    public void adminCounterWiseReport() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnAdminCounterWiseReport.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 10)
+    public void adminAgentWiseReport() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnAdminAgentWiseReport.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 11)
+    public void adminUserWiseReport() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnAdminUserWiseReport.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 12)
+    public void adminReservationReport() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnAdminReservationReport.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 13)
+    public void adminCoachWiseReportV2() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnAdminCoachWiseReport.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 14)
+    public void selectCompanyDropDown() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnSelectCompany.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 15)
+    public void companyNameSelect() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnCompanyNameSelect.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 16)
+    public void adminCoachWiseDataFilter() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnFilter.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 17)
+    public void adminCoachWiseSeatTicketDataFilter() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnViewSeatTicket.click();
+        Thread.sleep(1000);
+    }
+
+    //***** Company Deploy *****//
+    @Test(priority = 18)
+    public void clickCompanyDeploy2() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnClickCompanyDeploy.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 19)
+    public void clickNextButton() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,350)", "");
+//        Thread.sleep(5000);
+//        adminPanel.btnNext.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 20)
+    public void manageCompany2() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnNext.click();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,350)", "");
+        adminPanel.btnManage.click();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 21)
+    public void viewTrips() throws InterruptedException {
+        adminPanel = new AdminPanel(driver);
+        adminPanel.btnTrips.click();
+        adminPanel.btnAddNew.isDisplayed();
+        Thread.sleep(1000);
+    }
+
+    @Test(priority = 22)
+    public void clickAddNew() throws InterruptedException {
+        adminPanel.btnAddNew.click();
+        adminPanel.btnGoBack.isDisplayed();
+        Thread.sleep(1000);
+    }
+    @Test(priority = 23)
+    public void createTrip() throws InterruptedException {
+        adminPanel.clickRoutDropDown.click();
+        Thread.sleep(500);
+        adminPanel.setRouteName.click();
+        Thread.sleep(500);
+        adminPanel.clickFromDate.click();
+        Thread.sleep(500);
+        adminPanel.setFromDate.click();
+        Thread.sleep(500);
+        adminPanel.clickToDate.click();
+        Thread.sleep(500);
+        ///adminPanel.setToDate.click();
+//        Thread.sleep(500);
+//        adminPanel.btnGoBack.isDisplayed();
+    }
+
+    @Test(enabled = false)
+    public void doCountermanLoginWithValidCredentials() throws IOException, ParseException, InterruptedException {
+        adminPanel=new AdminPanel(driver);
+        utilsCounterman =new UtilsCounterman();
+        utilsCounterman.getUserCredentials(2);
+        //driver.get("https://intercity.jatriweb.team/");
+        boolean isCountermanLogoutFound= adminPanel.doCountermanLoginWithValidCredentials(utilsCounterman.getPhone(),utilsCounterman.getPassword());
+        Assert.assertEquals(isCountermanLogoutFound,true);
+        adminPanel.btnCountermanLogoutDisplayed.click();
+        Thread.sleep(3000);
+        adminPanel.btnCountermanLogout.click();
+
+    }
+}
